36edcc3134c6588e3f636825c86f7c8b25725d58
Kaede <149133145+K4edehara@users.noreply.github.com>
2024-11-11 15:03:33 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('black-btn').addEventListener('click', () => {
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+});
+
+document.getElementById('green-btn').addEventListener('click', () => {
+    document.body.style.backgroundColor = 'darkgreen';
+    document.body.style.color = 'white';
+});
+
+document.getElementById('beige-btn').addEventListener('click', () => {
+    document.body.style.backgroundColor = 'beige';
+    document.body.style.color = 'black';
+});
